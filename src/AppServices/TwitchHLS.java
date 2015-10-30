75ed9a43c32bbb54f72f29804427f9ad4aebde83
LukeG317 <lukas.greiner@me.com>
2015-10-30 08:36:20 +0100

    Changing UI for Windows

--- src/AppServices/TwitchHLS.java
+++ src/AppServices/TwitchHLS.java
@@ -6,7 +6,11 @@
 package AppServices;
 
 import AppGUI.MainFrame;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -18,6 +22,13 @@ public class TwitchHLS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        if(System.getProperty("system.os").startsWith("Windows")){
+            try {
+                UIManager.setLookAndFeel("com.alee.laf.WebLookAndFee");
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+                Logger.getLogger(TwitchHLS.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
         Following follow = new Following();
         SwingUtilities.invokeLater(new Runnable() {
             @Override
